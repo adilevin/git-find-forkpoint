4d685a4a5be87bab89e9cea317ea2fa015f49b7f
Adi Levin <alevin@aligntech.com>
2016-01-29 13:15:25 +0200

    Add error messages and usage instructions + examples

--- find-fork-point.sh
+++ find-fork-point.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
-commit_to_compare_with=d67e
-commit_range=1cb1d..e172
+if test "$#" -ne 2; then
+  echo "illegal number of parameters"
+  echo "Usage: "
+  echo "   find-fork-point <range of commits> <commit reference>"
+  echo "Examples: "
+  echo "   find-fork-point 1cb1d..e172 d67e"
+  echo "   find-fork-point master~10..master 27c235cecee2"
+  exit 1
+fi
+
+commit_range=$1
+commit_to_compare_with=$2
 
 list_of_commits=($(git rev-list $commit_range))
 num_of_commits=${#list_of_commits[@]}
 minimal_diff_count=100000000
 
+if test $num_of_commits -eq 0; then
+  echo
+  echo Error: Did not find any commits in the range $commit_range
+  echo
+  exit 1
+fi
+
 echo
-echo Found $num_of_commits commits in the range $commit_range
+echo Info: Found $num_of_commits commits in the range $commit_range
 echo
 
 count_lines_of_diff() { git diff $1 $2 | wc -l; }
